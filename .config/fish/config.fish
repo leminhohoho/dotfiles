3978c6728675aee8234261eb94ed71f8d0d82280
leminhohoho <leminhnguyenai@gmail.com>
2025-05-13 20:26:55 +0700

    Update dotfiles

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -4,10 +4,10 @@ function fish_prompt
     string join '' -- (set_color green) '[' $USER ']' (set_color blue) (prompt_pwd) (set_color normal) '> '
 end
 
-# WARNING: Don't commit this shit
 set -e OPENAI_API_KEY
 set -x ELECTRON_OZONE_PLATFORM_HINT wayland
 set -x QT_QPA_PLATFORM wayland
+set -x GDK_BACKEND wayland
 
 set -x MANPAGER "nvim +Man!"
 set -x FZF_DEFAULT_OPTS "--color 'bg:#181818'"
@@ -23,8 +23,6 @@ fish_add_path /home/linuxbrew/.linuxbrew/bin
 fish_add_path ~/.cargo/bin
 fish_add_path ~/.local/bin
 
-starship init fish | source
-
 function dotfiles
     /usr/bin/git --git-dir=$HOME/dot-files/ --work-tree=$HOME $argv
 end
@@ -35,9 +33,9 @@ function brightness
    /bin/brightnessctl -d "apple-panel-bl" set $argv 
 end
 
-function pdf
-    /bin/zathura $argv& && disown
-end
+# function pdf
+#     /bin/zathura $argv& && disown
+# end
 
 function run_electron
     $argv --appimage-extract-and-run --js-flags="--nodecommit_pooled_pages" & 
---
+++ setup.sh
@@ -0,0 +1,330 @@
+#!/usr/bin/bash
+
+fedora_version=$(sudo cat /etc/fedora-release)
+
+cat << EOF
+
+Asahi linux minimal setup
+$fedora_version
+
+EOF
+
+echo "Press ENTER to continue"
+read -n 1 -s -r -p ""
+
+###################
+### Wifi set up ###
+###################
+
+cat << EOF
+
+WIFI connection
+
+EOF
+
+if ping -c1 -w1 1.1.1.1 >/dev/null 2>&1; then
+	echo "Already connected to the internet, continue the setup process"
+else
+	echo "List of available network"
+	nmcli device wifi list
+
+	read -p "Please enter the name of the wifi to connect to: " wifi
+
+	nmcli device wifi connect "$wifi" --ask
+fi
+
+########################################################
+### Checking for update & installing essential tools ###
+########################################################
+
+echo -e "Checking for update\n"
+sudo dnf update
+
+echo -e "Installing essential tools\n"
+sudo dnf install fastfetch htop neovim fish git openssh-server ufw brightnessctl
+
+#########################
+### Set up SSH server ###
+#########################
+
+sudo systemctl enable --now sshd
+sudo systemctl status sshd
+sudo ufw allow ssh
+sudo systemctl start ufw
+sudo ufw status
+
+
+##########################
+### Swap configuration ###
+##########################
+
+cat << EOF
+
+Swap configuration
+
+EOF
+
+# Changing swappiness
+swappiness=$(sudo cat /proc/sys/vm/swappiness)
+echo "Current swapiness: $swappiness"
+echo "Do you want to change your swapiness"
+read -p "[y/n] " decision
+
+while [ "$decision" != "y" ] && [ "$decision" != "n" ]
+do
+	read -p "[y/n] " decision
+done
+
+if [[ "$decision" == "n" ]]; then
+	echo "Swappiness will be kept at $swappiness"
+else
+	# read -p "Enter a new swappiness value: " swappiness
+	# sudo sysctl vm.swappiness="$swappiness"
+	echo "You will be drop into the editor to edit the swappiness, press ENTER to continue"
+	read -n 1 -s -r -p ""
+	sudo nvim /etc/sysctl.conf
+	sudo sysctl -p
+	echo "Your new swappiness is:$(sudo cat /proc/sys/vm/swappiness)"
+fi
+echo -e "\nYour current swap size:"
+swapon -s
+
+
+echo "Do you want to change your swapfile size ?"
+read -p "[y/n] " decision
+
+while [ "$decision" != "y" ] && [ "$decision" != "n" ]
+do
+	read -p "[y/n] " decision
+done
+
+if [[ "$decision" == "n" ]]; then
+	echo "Skipping swapfile modification"
+else
+	read -p "Please enter the new swap size: " swap_size
+	
+	while true;
+	do 
+		if [[ "$swap_size" =~ ^-?[0-9]+GB$ ]]; then
+			break
+		fi
+
+		read -p "Invalid format, please enter again: " swap_size
+	done
+
+
+	sudo swapoff /var/swap/swapfile >/dev/null 2>&1
+	sudo rm /var/swap/swapfile >/dev/null 2>&1
+	sudo fallocate -l "$swap_size" /var/swap/swapfile >/dev/null 2>&1
+	sudo chmod 600 /var/swap/swapfile >/dev/null 2>&1
+	sudo mkswap /var/swap/swapfile >/dev/null 2>&1
+	sudo swapon /var/swap/swapfile >/dev/null 2>&1
+	echo "New swap size: "
+	swapon -s
+fi
+
+######################
+### Changing shell ###
+######################
+
+if [[ "$SHELL" != "/bin/fish"  ]] && [[ "$SHELL" != "/usr/bin/fish" ]]; then
+	chsh -s "$(which fish)"
+fi
+
+######################
+### Setting up git ###
+######################
+
+cat << EOF
+
+Setting up git
+
+EOF
+
+if [[ -z $(git config --get user.name) ]]; then
+	read -p "Enter your github username: " github_username
+	while [ github_username == "" ]
+	do
+		read -p "No name specified, please enter again: " github_username
+	done
+	git config --global user.name "$github_username"
+fi
+
+if [[ -z $(git config --get user.email) ]]; then
+	read -p "Enter your github email: " github_email
+	while [ github_email == "" ]
+	do
+		read -p "No emaill specified, please enter again: " github_email
+	done
+	git config --global user.email "$github_email"
+fi
+
+echo "Your github name is: $(git config --get user.name)"
+echo "Your github email is: $(git config --get user.email)"
+
+# SSH key setup
+keys=$(ls ~/.ssh/id_* 2>/dev/null | grep -v '\.pub')
+
+if [[ -z "$keys" ]]; then
+	ssh-keygen
+
+	echo "SSH key is created"
+else
+	echo "SSH key is already created"
+fi
+
+sudo cat ~/.ssh/id_ed25519.pub > /tmp/key.txt
+echo -e "Your SSH key is:\n$(cat ~/.ssh/id_ed25519.pub)"
+read -n 1 -s -r -p "Copy this key to your github page, press ENTER to continue"
+echo ""
+
+ssh -T git@github.com
+
+#######################
+### Languages setup ###
+#######################
+
+# Setting up snapd
+echo "Setting up languages"
+echo "Installing snapd..."
+if ! command -v snap &> /dev/null; then
+	sudo dnf install snapd
+	read -n 1 -s -r -p "In order for snap to work, you have to logout and login again, press ENTER now to logout, after that, rerun the script again"
+	loginctl terminate-user "$USER"
+	sudo ln -s /var/lib/snapd/snap /snap
+	sudo snap install hello-world
+else 
+	echo "snapd already installed"
+fi
+hello-world
+
+# Installing go
+echo "Installing Go..."
+if ! command -v go &> /dev/null; then
+	sudo snap install go --classic
+else 
+	echo "Go already installed"
+fi
+echo "Go version: $(go version)"
+
+# Installing zig
+echo "Installing Zig..."
+if ! command -v zig &> /dev/null; then
+	sudo snap install zig --beta --classic
+else 
+	echo "Zig already installed"
+fi
+echo "Zig version: $(zig version)"
+
+# Installing node
+echo "Installing Node..."
+if ! command -v node &> /dev/null; then
+	sudo snap install node --classic
+else 
+	echo "Node already installed"
+fi
+echo "Node version: $(node --version)"
+
+# Installing java
+echo "Installing Java..."
+if ! command -v java &> /dev/null; then
+	sudo dnf install java-latest-openjdk-devel.aarch64
+else 
+	echo "Java already installed"
+fi
+java -version
+
+# Installing rust
+echo "Installing Rust..."
+if ! command -v  rustc &> /dev/null; then
+	sudo snap install rustup --classic
+	rustup default stable
+else 
+	echo "Rust already installed"
+fi
+echo "Rust version: $(rustc --version)"
+
+######################################################################
+### Installing personalized tools for dotfiles related tools (TUI) ###
+######################################################################
+sudo dnf install fzf ripgrep
+# install starhip
+if ! command -v starship &> /dev/null; then
+    curl -sS https://starship.rs/install.sh | sh
+else 
+    echo "Starship has already been installed"
+fi
+starship --version
+
+#########################
+### Initiate dotfiles ###
+#########################
+
+# Download dotfile manager
+echo "Downloading doffy dotfiles manager..."
+if ! command -v  doffy &> /dev/null; then
+	go install github.com/leminhohoho/doffy@latest
+else 
+	echo "Doffy already installed"
+fi
+
+# Download the dotfiles
+if ! ls $HOME/dotfiles &> /dev/null; then
+	git clone git@github.com:leminhohoho/dotfiles.git
+else
+	echo "dotfiles already installed"
+fi
+
+# Remove the default config files
+rm -rf $HOME/.config/fish
+
+# Create the symlinks
+$HOME/go/bin/doffy $HOME/dotfiles
+
+###########################
+### Installing Hyprland ###
+###########################
+
+# Install kitty and Hyprland and its dependencies
+echo "Installing Hyprland and its dependencies"
+sudo dnf install kitty hyprland thunar rofi waybar
+
+# Installing hyprland utilities
+
+# Installing fonts 
+FONT_GROUP_NAME="JetBrainsMono Nerd Font"
+    if ! fc-list | grep -q "$FONT_GROUP_NAME"; then 
+    mkdir -p "$HOME/.local/share/fonts"
+    cd "$HOME/.local/share/fonts"
+    curl -fLo "JetBrainsMonoNerdFont.zip" -L "https://github.com/ryanoasis/nerd-fonts/releases/download/v3.4.0/JetBrainsMono.zip"
+    unzip "JetBrainsMonoNerdFont.zip"
+    rm "JetBrainsMonoNerdFont.zip"
+    fc-cache -fv
+else
+    echo "Font $FONT_GROUP_NAME already installed"
+fi
+
+##############################################
+### Installing other quality of life tools ###
+##############################################
+
+# Installing browsers
+echo "Installing browser"
+sudo dnf install qutebrowser chromium
+
+# Installing dev tools
+echo "Installing dev tools"
+## Installing tmux
+sudo dnf install tmux
+tmux -V
+
+if ls $HOME/.tmux/plugins/tpm &> /dev/null; then
+    git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm
+else
+	echo "tmux plugin manager already installed"
+fi
+
+# FIX: Fix Waybar rendering issue with color and hover
+# FIX: Fix keyboard not being able to use Win key
+# FIX: Fix USB devices not detected automatically
+# FIX: Fix neovim breacumb doesn't follow the transparent background
