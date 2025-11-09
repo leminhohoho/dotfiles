07c2be191ad9070cda4baeb3fdc3a60135abdfdb
leminhohoho <leminhnguyenai@gmail.com>
2025-11-09 15:33:14 +0700

    Modify setup script

--- setup.sh
+++ setup.sh
@@ -2,8 +2,8 @@
 
 export XDG_CONFIG_HOME="$HOME"/.config
 
-ln -sf "$PWD/nvim" "$XDG_CONFIG_HOME"/nvim
-ln -sf "$PWD/fish" "$XDG_CONFIG_HOME"/fish
+ln -sf "$PWD/.config/nvim" "$XDG_CONFIG_HOME"/nvim
+ln -sf "$PWD/.config/fish" "$XDG_CONFIG_HOME"/fish
 
 echo "All packages are installed"
 
